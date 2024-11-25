aa7f05785e4befbfda42c8fa1a3422ac06dadcf3
ikhsansd <snsssjsjsb@gmail.com>
2024-11-25 15:17:33 +0700

    ikhsan

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Box, Heading, Button} from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <Box p='40'>
+            <Heading> Home</Heading>
+            <Button color='teal'> Click Me</Button>
+        </Box>
+    );
+
+}
